59f93cfa5e5bad67645c6845c9deef48873c0037
Shankar Reddy <96974151+AdityaReddyS@users.noreply.github.com>
2025-02-14 16:27:18 +0530

    changes are added in application,properties
    changes are added in Test Runner class

--- src/main/java/com/shankar/runner/TestRunner.java
+++ src/main/java/com/shankar/runner/TestRunner.java
@@ -24,6 +24,7 @@ System.out.println(msg);*/
 
        List<HondaBike> list=service.getallBikes();
        list.forEach(System.out::print);
+       System.out.println("-------Executing For Loop-----------");
         System.out.println("------------------------------");
        list.forEach(b->{
            System.out.println(b);
